2bd0a724c805d79122dd7dd99c82d85e456f00cc
Christian Jahnel <christian.jahnel257@gmail.com>
2024-05-20 17:25:58 -0400

    Refactored server into a JSON API, htmx can be run statically

--- src/app.py
+++ src/app.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template, request
 from backend import answer
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
 
 @app.post("/clicked")
 def user():
     user_msg = request.form["userInput"]
     assistant_msg = answer(user_msg)
 
-    return render_template(
-        "filler.html",
-        user_msg=user_msg,
-        assistant_msg=assistant_msg
-    )
+    return {
+        "userMsg": user_msg,
+        "assistantMsg": assistant_msg
+    }
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
