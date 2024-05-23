bb87c972808e8a9b57413365bf815ffa45e68e61
Christian Jahnel <christian.jahnel257@gmail.com>
2024-05-23 17:18:41 -0400

    Integrated APIs and emulators; deploying to see if API keys work in Prod

--- functions/backend.py
+++ functions/backend.py
@@ -1,13 +1,11 @@
 import json
-import os
 
 from openai import OpenAI
 from pinecone import Pinecone
 
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-openai_client = OpenAI()
+from main import OPENAI_API_KEY, PINECONE_API_KEY
 
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
 pc = Pinecone(api_key=PINECONE_API_KEY)
 
 messages = [{
@@ -17,31 +15,31 @@ messages = [{
 }]
 
 def answer(content: str) -> str:
-    tools = [{
-        "type": "function",
-        "function": {
-            "name": "searchDocs",
-            "description": "Retrieve information about Jahnel Group from company documents",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "summary": {
-                        "type": "string",
-                        "description": "The keywords in the user's question",
-                    }
-                },
-                "required": ["summary"]
-            }
-        }
-    }]
+    # tools = [{
+    #     "type": "function",
+    #     "function": {
+    #         "name": "searchDocs",
+    #         "description": "Retrieve information about Jahnel Group from company documents",
+    #         "parameters": {
+    #             "type": "object",
+    #             "properties": {
+    #                 "summary": {
+    #                     "type": "string",
+    #                     "description": "The keywords in the user's question",
+    #                 }
+    #             },
+    #             "required": ["summary"]
+    #         }
+    #     }
+    # }]
 
     messages.append({"role": "user", "content": content})
 
     completion = openai_client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=messages,
-        tools=tools,
-        tool_choice="auto"
+        messages=messages
+        # tools=tools,
+        # tool_choice="auto"
     )
     
     message = completion.choices[0].message
@@ -50,34 +48,34 @@ def answer(content: str) -> str:
         messages.append({"role": "assistant", "content": message.content})
         return message.content
     
-    function = message.tool_calls[0].function
+    # function = message.tool_calls[0].function
 
-    if function.name == "searchDocs":
-        arguments = json.loads(function.arguments)
-        summary = arguments["summary"]
-        documents = searchDocs(summary).get("documents")[0]
-        result = " ".join(documents)
-        messages.append({
-            "role": "function",
-            "name": "searchDocs",
-            "content": result
-        })
-    else:
-        raise ValueError(f"Unknown function: {function.name}")
+    # if function.name == "searchDocs":
+    #     arguments = json.loads(function.arguments)
+    #     summary = arguments["summary"]
+    #     documents = searchDocs(summary).get("documents")[0]
+    #     result = " ".join(documents)
+    #     messages.append({
+    #         "role": "function",
+    #         "name": "searchDocs",
+    #         "content": result
+    #     })
+    # else:
+    #     raise ValueError(f"Unknown function: {function.name}")
     
-    completion = openai_client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
+    # completion = openai_client.chat.completions.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=messages
+    # )
 
-    # repeating code, should probably abstract this as a function
-    message = completion.choices[0].message
+    # # repeating code, should probably abstract this as a function
+    # message = completion.choices[0].message
 
-    messages.append({"role": "assistant", "content": message.content})
-    return message.content
+    # messages.append({"role": "assistant", "content": message.content})
+    # return message.content
     
-def searchDocs(summary: str) -> QueryResult:
-    pass
+# def searchDocs(summary: str) -> QueryResult:
+#     pass
     # return collection.query(
     #     query_texts=[summary],
     #     n_results=3
--- functions/main.py
+++ functions/main.py
@@ -1,13 +1,19 @@
 from firebase_functions import https_fn
-from firebase_admin import initialize_app
+from firebase_functions.params import SecretParam
 
-initialize_app()
+# from firebase_admin import initialize_app
 
+# initialize_app()
 
-@https_fn.on_request()
+OPENAI_API_KEY = SecretParam("OPENAI_API_KEY")
+PINECONE_API_KEY = SecretParam("PINECONE_API_KEY")
+
+@https_fn.on_request(secrets=[OPENAI_API_KEY, PINECONE_API_KEY])
 def new_message(req: https_fn.Request) -> https_fn.Response:
+    from backend import answer
+    
     user_msg = req.form["userInput"]
-    # assistant_msg = answer(user_msg)
+    assistant_msg = answer(user_msg)
 
     return {
         "userMsg": user_msg,
