590156bfeee9adebebcb64e473c7395abbd6b45e
Christian Jahnel <christian.jahnel257@gmail.com>
2024-05-23 13:56:35 -0400

    Changed to test Github Action

--- functions/main.py
+++ functions/main.py
@@ -9,8 +9,7 @@ def new_message(req: https_fn.Request) -> https_fn.Response:
     user_msg = req.form["userInput"]
     # assistant_msg = answer(user_msg)
 
-    # return https_fn.Response("Hello world!")
     return {
         "userMsg": user_msg,
-        "assistantMsg": "Hello, world!"
+        "assistantMsg": "Hello, World!"
     }
