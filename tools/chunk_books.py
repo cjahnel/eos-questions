fe839f4e31af1baf2f0a9360afb2522a30fa1305
Christian Jahnel <christian.jahnel257@gmail.com>
2024-05-30 13:38:19 -0400

    Revised scripts for chunking and embedding to accomodate the subsections in Traction

--- tools/chunk_books.py
+++ tools/chunk_books.py
@@ -4,51 +4,64 @@ import tiktoken
 
 def chunk_book(book_name: str) -> None:
     book_dir = os.path.join("books", book_name)
-    book_content_path = f"{os.path.join(book_dir, book_name)}-content.txt"
+    book_content_file = f"{os.path.join(book_dir, book_name)}-content.txt"
 
-    with open(book_content_path, "r") as book:
+    with open(book_content_file, "r") as book:
         chunks = book.read().split("\n\n")
 
-        is_chapter_headline = False
         section_number = 0
+        subsection_number = 0
 
         for chunk in chunks:
-            # TODO: (Later version?) Ask GPT-4 to describe the images and upload them to the index
-            # tagged as images and can call with another function upon request, images stored in Firebase Storage
+            # TODO? Tag images with locator in Firebase Storage and upload their embedded descriptions to Pinecone
+            # Can be returned upon request for a specific image
             chunk_lines = [line.strip() for line in chunk.splitlines() if not re.match(r"%%.+%%", line)]
 
-            first_line = chunk_lines[0]
+            heading = chunk_lines[0]
 
             chapter_regex = re.compile(r"(CHAPTER) (\d+)")
             appendix_regex = re.compile(r"(APPENDIX) ([ABC]):")
 
-            if first_line == "INTRODUCTION":
-                is_chapter_headline = True
-                chapter_dir = "intro"
-            elif chapter_regex.match(first_line):
-                is_chapter_headline = True
-                chapter_number = int(chapter_regex.search(first_line).group(2))
-                chapter_dir = f"chapter{chapter_number}"
+            if heading == "INTRODUCTION":
+                chunk_level = "chapter"
+                chapter_name = "intro"
+            elif chapter_regex.match(heading):
+                chunk_level = "chapter"
+                chapter_number = int(chapter_regex.search(heading).group(2))
+                chapter_name = f"chapter{chapter_number}"
                 section_number = 0
-            elif appendix_regex.match(first_line):
-                is_chapter_headline = True
-                appendix_letter = appendix_regex.search(first_line).group(2)
-                chapter_dir = f"appendix{appendix_letter}"
+            elif appendix_regex.match(heading):
+                chunk_level = "chapter"
+                appendix_letter = appendix_regex.search(heading).group(2)
+                chapter_name = f"appendix{appendix_letter}"
+            elif book_name == "traction" and heading in subsection_headings:
+                chunk_level = "subsection"
+                subsection_number += 1
+                subsection_name = f"subsection{subsection_number}"
             else:
-                is_chapter_headline = False
+                chunk_level = "section"
                 section_number += 1
+                section_name = f"section{section_number}"
+                subsection_number = 0
 
-            chapter_dir_path = os.path.join(book_dir, "chunks", chapter_dir)
+            chapter_dir = os.path.join(book_dir, "chunks", chapter_name)
 
-            if is_chapter_headline:
-                os.makedirs(chapter_dir_path, exist_ok=True)
-                chunk_path = os.path.join(chapter_dir_path, chapter_dir)
+            if chunk_level == "chapter":
+                os.makedirs(chapter_dir, exist_ok=True)
+                chunk_file = os.path.join(chapter_dir, chapter_name)
+            elif chunk_level == "section":
+                if book_name == "traction" and heading in section_headings_with_subsections:
+                    section_dir = os.path.join(chapter_dir, section_name)
+                    os.makedirs(section_dir, exist_ok=True)
+                    chunk_file = os.path.join(section_dir, section_name)
+                else:
+                    chunk_file = os.path.join(chapter_dir, section_name)
+            elif chunk_level == "subsection":
+                chunk_file = os.path.join(chapter_dir, section_name, subsection_name)
             else:
-                chunk_path = os.path.join(chapter_dir_path, f"section{section_number}")
-            
-            # TODO? Store a synopsis of each chunk from GPT-4o
+                raise Exception("Invalid chunk level")
 
-            with open(f"{chunk_path}.txt", "w+") as outfile:
+            with open(f"{chunk_file}.txt", "w+") as outfile:
                 enc = tiktoken.get_encoding("cl100k_base")
                 chunk_text = "\n".join(chunk_lines)
                 encoding = enc.encode(chunk_text)
@@ -91,7 +104,72 @@ get_a_grip_outline = """
         DAY TWO
     CHAPTER 8 TRACTION
 """
-    
+
+section_headings_with_subsections = [
+    "WHAT ARE YOUR CORE VALUES?",
+    "WHAT IS YOUR CORE FOCUS?",
+    "WHAT IS YOUR MARKETING STRATEGY?",
+    "WHAT IS YOUR THREE-YEAR PICTURE?",
+    "WHAT IS YOUR ONE-YEAR PLAN?",
+    "WHAT ARE YOUR ISSUES?",
+    "THE ISSUES SOLVING TRACK",
+    "THE EOS QUARTERLY MEETING PULSE",
+    "THE EOS ANNUAL MEETING PULSE",
+    "THE LEVEL 10 WEEKLY MEETING AGENDA",
+    "DISCOVERIES, POTHOLES, AND DELAYS ON THE GRAND JOURNEY"
+]
+
+subsection_headings = [
+    "STEP 1",
+    "STEP 2",
+    "STEP 3",
+    "STEP 4",
+    "HOW TO DETERMINE YOUR CORE FOCUS",
+    "YOUR TARGET MARKET",
+    "YOUR THREE UNIQUES",
+    "YOUR PROVEN PROCESS",
+    "YOUR GUARANTEE",
+    "PAINT THE THREE-YEAR PICTURE",
+    "HOW TO CREATE YOUR ONE-YEAR PLAN",
+    "HOW TO IDENTIFY YOUR ISSUES",
+    "STEP 1: IDENTIFY",
+    "STEP 2: DISCUSS",
+    "STEP 3: SOLVE",
+    "Segue",
+    "Review Previous Quarter",
+    "You have one of three options with incomplete Rocks:",
+    "Review the V/TO",
+    "Establish Next Quarter’s Rocks",
+    "Tackle Key Issues",
+    "Next Steps",
+    "Conclude",
+    "Segue",
+    "Review Previous Year",
+    "Team Health Building",
+    "SWOT/Issues List",
+    "V/TO (Through One-Year Plan)",
+    "Segue",
+    "Scorecard",
+    "Rock Review",
+    "Customer/Employee Headlines",
+    "To-Do List",
+    "IDS",
+    "Conclude",
+    "ROLLING OUT EOS TO YOUR COMPANY",
+    "YOU CAN ONLY MOVE AT YOUR OWN SPEED",
+    "WHY IT WORKS",
+    "THE “CLICK\"",
+    "YOU HAVE TO DO THE WORK",
+    "STAY COMMITTED TO THE 90-DAY WORLD",
+    "YOU WILL HIT THE CEILING AGAIN",
+    "BIGGER ISN’T ALWAYS BETTER",
+    "COMPARTMENTALIZING",
+    "SAME-PAGE MEETINGS",
+    "TAKE A CLARITY BREAK",
+    "SHINY STUFF",
+    "THE ROAD TO HANA"
+]
+
 traction_outline = """
     INTRODUCTION
     CHAPTER 1 THE ENTREPRENEURIAL OPERATING SYSTEM
@@ -182,7 +260,7 @@ traction_outline = """
             Review Previous Quarter
             You have one of three options with incomplete Rocks:
             Review the V/TO
-            Establish Next Quarter's Rocks
+            Establish Next Quarter’s Rocks
             Tackle Key Issues
             Next Steps
             Conclude
@@ -196,7 +274,7 @@ traction_outline = """
         THE BUILDUP
         THE WEEKLY MEETING PULSE
         ALWAYS A LEVEL 10 MEETING
-        THE EOS WEEKLY MEETING PULSE
+        THE LEVEL 10 WEEKLY MEETING AGENDA
             Segue
             Scorecard
             Rock Review
@@ -204,20 +282,20 @@ traction_outline = """
             To-Do List
             IDS
             Conclude
-            THE FIVE POINTS OF THE WEEKLY MEETING PULSE
-            THE WEEKLY MEETING ROLLOUT
-            MEETING PULSE ACTIONS
+        THE FIVE POINTS OF THE WEEKLY MEETING PULSE
+        THE WEEKLY MEETING ROLLOUT
+        MEETING PULSE ACTIONS
     CHAPTER 9 PUTTING IT ALL TOGETHER: THE GRAND JOURNEY
         ORGANIZATIONAL CHECKUP
         DISCOVERIES, POTHOLES, AND DELAYS ON THE GRAND JOURNEY
             ROLLING OUT EOS TO YOUR COMPANY
             YOU CAN ONLY MOVE AT YOUR OWN SPEED
             WHY IT WORKS
-            THE "CLICK"
+            THE “CLICK"
             YOU HAVE TO DO THE WORK
             STAY COMMITTED TO THE 90-DAY WORLD
             YOU WILL HIT THE CEILING AGAIN
-            BIGGER ISN'T ALWAYS BETTER
+            BIGGER ISN’T ALWAYS BETTER
             COMPARTMENTALIZING
             SAME-PAGE MEEETINGS
             TAKE A CLARITY BREAK
@@ -319,5 +397,5 @@ wth_is_eos_outline = """
 
 if __name__ == "__main__":
     chunk_book("get-a-grip")
-    # chunk_book("traction")
+    chunk_book("traction")
     chunk_book("wth-is-eos")
--- tools/vectorize_chunks.py
+++ tools/vectorize_chunks.py
@@ -9,44 +9,56 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
-def upload_chunk(book_dir: str) -> None:
-    chunks_dir_path = os.path.join("books", book_dir, "chunks")
+def tag_chunk(chunk_file: str, book_name: str, chapter_name: str, section_name: str, subsection_name: str = None):
+    id = f"{book_name}#{chapter_name}#{section_name}"
+    if subsection_name:
+        id += f"#{subsection_name}"
+
+    if book_name == "get-a-grip":
+        book_name = "Get a Grip"
+    elif book_name == "traction":
+        book_name = "Traction"
+    elif book_name == "wth-is-eos":
+        book_name = "What the Heck is EOS?"
+
+    chapter_regex = re.compile(r"(chapter)(\d+)")
+    appendix_regex = re.compile(r"(appendix)([ABC])")
+
+    if chapter_name == "intro":
+        chapter_name= "Introduction"
+    elif chapter_regex.match(chapter_name):
+        chapter_number = int(chapter_regex.search(chapter_name).group(2))
+        chapter_name = f"Chapter {chapter_number}"
+    elif appendix_regex.match(chapter_name):
+        appendix_letter = appendix_regex.search(chapter_name).group(2)
+        chapter_name = f"Appendix {appendix_letter}"
+
+    with open(chunk_file, "r") as chunk:
+        chunk_text = chunk.read()
+
+    return {
+        "id": id,
+        "metadata": {"book": book_name, "chapter": chapter_name, "text": chunk_text}
+    }
+
+def upload_chunk(book_name: str) -> None:
+    chunks_dir = os.path.join("books", book_name, "chunks")
 
     chunks = []
 
-    for chapter_dir in os.listdir(chunks_dir_path):
+    for chapter_name in os.listdir(chunks_dir):
+        chapter_dir = os.path.join(chunks_dir, chapter_name)
+        for section_name in os.listdir(chapter_dir):
+            section = os.path.join(chapter_dir, section_name)
 
-        chapter_dir_path = os.path.join(chunks_dir_path, chapter_dir)
-
-        for section_file in os.listdir(chapter_dir_path):
-            chunk_path = os.path.join(chapter_dir_path, section_file)
-
-            with open(chunk_path, "r") as chunk_file:
-                chunk_text = chunk_file.read()
-
-                if book_dir == "get-a-grip":
-                    book_name = "Get a Grip"
-                elif book_dir == "traction":
-                    book_name = "Traction"
-                elif book_dir == "wth-is-eos":
-                    book_name = "What the Heck is EOS?"
-
-                chapter_regex = re.compile(r"(chapter)(\d+)")
-                appendix_regex = re.compile(r"(appendix)([ABC])")
-
-                if chapter_dir == "intro":
-                    chapter_name= "Introduction"
-                elif chapter_regex.match(chapter_dir):
-                    chapter_number = int(chapter_regex.search(chapter_dir).group(2))
-                    chapter_name = f"Chapter {chapter_number}"
-                elif appendix_regex.match(chapter_dir):
-                    appendix_letter = appendix_regex.search(chapter_dir).group(2)
-                    chapter_name = f"Appendix {appendix_letter}"
-
-                chunks.append({
-                    "id": f"{book_dir}#{chapter_dir}#{section_file.removesuffix(".txt")}",
-                    "metadata": {"book": book_name, "chapter": chapter_name, "text": chunk_text}
-                })
+            if os.path.isdir(section):
+                for subsection_name in os.listdir(section):
+                    subsection_file = os.path.join(section, subsection_name)
+                    tagged_subsection = tag_chunk(subsection_file, book_name, chapter_name, section_name, subsection_name)
+                    chunks.append(tagged_subsection)
+            else:
+                tagged_chunk = tag_chunk(section, book_name, chapter_name, section_name)
+                chunks.append(tagged_chunk)
 
     input = [chunk["metadata"]["text"] for chunk in chunks]
 
@@ -61,8 +73,8 @@ def upload_chunk(book_dir: str) -> None:
     for i, chunk in enumerate(chunks):
         chunk.update({"values": data[i].embedding})
     
-    if len(chunks) > 100:
-        raise Exception("Too many chunks to load at once, should be done in a batch")
+    # if len(chunks) > 100:
+        # raise Exception("Too many chunks to load at once, should be done in a batch")
 
     index = pc.Index("eos-questions")
 
@@ -73,5 +85,5 @@ def upload_chunk(book_dir: str) -> None:
 
 if __name__ == "__main__":
     upload_chunk("get-a-grip")
-    # upload_chunk("traction")
+    upload_chunk("traction")
     upload_chunk("wth-is-eos")
